cb31e7ed3cf7b69c795420a6ebe19d1691288407
AkibRiad <akibriad652@gmail.com>
2024-09-15 20:15:41 +0600

    ternary advanced

--- starting.js
+++ starting.js
@@ -9,4 +9,13 @@
 //     console.log("You are not eligable for voting");
 // }
 
-age > 18 ? console.log("vote dao giye")  : console.log("ghumiye poro when") ; 
+// age > 18 ? console.log("vote dao giye")  : console.log("ghumiye poro when") ; 
+
+ let price = 500;
+const isLeader = true ;
+
+// price = isLeader === true ? 50 : price+200;
+// console.log(price)
+
+price = isLeader === true ? price > 400 ? price/2 : 0 : price+5000;
+console.log(price)
