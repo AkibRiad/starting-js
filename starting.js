a2103552a303394fe81d67396a7dd17cc8e353f7
AkibRiad <akibriad652@gmail.com>
2024-09-15 19:56:13 +0600

    ternary

---
+++ starting.js
@@ -0,0 +1,12 @@
+// ternary
+
+
+ const age = 2;
+// if(age > 18){
+//     console.log("You Can vote for bangladesh");
+// }
+// else{
+//     console.log("You are not eligable for voting");
+// }
+
+age > 18 ? console.log("vote dao giye")  : console.log("ghumiye poro when") ; 
